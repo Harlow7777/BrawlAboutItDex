8b93a5afb516b1336e293fa82fb18b0a921f3fc0
Harlow7777 <Harlow_Jacob@outlook.com>
2021-03-17 19:41:39 -0500

    Update brawl.js

--- brawl.js
+++ brawl.js
@@ -44,7 +44,7 @@ function getCreatures()
 	console.log('Array before loading: ' + creatureElementArray.length);
 	creatureElementArray = JSON.parse(response);
 	console.log('Added json to array: ' + creatureElementArray.length);
-	sortByValue('Id');
+	sortByValue('stats.id');
 	console.log('SORTED ARRAY: ' + creatureElementArray.length);
     });
 };
