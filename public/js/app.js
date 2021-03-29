cd97d8a69da33240dc6e091246fe9779b095925c
Harlow7777 <Harlow_Jacob@outlook.com>
2021-03-29 11:03:48 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -101,7 +101,7 @@ function addCardToUserMetadata(user, authToken) {
     var options = {
         method: 'PATCH',
         url: 'https://harlow777.us.auth0.com/api/v2/users/' + user.sub,
-        headers: {'authorization': Bearer '+ authToken +','content-type': 'application/json'},
+        headers: {'authorization': 'Bearer ' + authToken,'content-type': 'application/json'},
         data: {
             user_metadata: {
                 creature_collection:'1000'
