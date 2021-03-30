acbf33630b62b6a8fd1944df73494080b673cac6
Harlow7777 <Harlow_Jacob@outlook.com>
2021-03-30 13:14:57 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -130,6 +130,7 @@ async function retrieveAuthAPIToken() {
     };
     axios.request(options).then(function (response) {
 	Object.keys(response.data).forEach(key => console.log(key + ": " + response.data[key]));
+	console.log("AUTH TOKEN: " + response.data[key]);
         return response.data[access_token];
     }).catch(function(error) {
         console.error(error);
